e22736b98aad2e5368b8bb238cfbb39842822ec6
Mariam Mohamed Ossama <mariamosama772@gmail.com>
2026-05-24 23:49:07 +0300

    Using Tag Helpers for Details action in Plans

---
+++ GymManagement/Controllers/PlansController.cs
@@ -0,0 +1,36 @@
+﻿using GymManagement.DbContexts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymManagement.Controllers
+{
+    public class PlansController : Controller
+    {
+        private readonly GymDbContext dbContext;
+        public PlansController()
+        {
+            dbContext = new GymDbContext();
+        }
+        // Index action
+        // GET Base_Url/Plans/Index -> Get all plans
+        public async Task<IActionResult> Index()
+        {
+            var plans = await dbContext.Plans.ToListAsync();
+            return View(plans);
+        }
+        // Details Action
+        // GET Base_Url/Plans/Details/{id} -> Get plan details by id
+        public async Task<IActionResult> Details(int id)
+        {
+            var plan = await dbContext.Plans.FindAsync(id);
+            if (plan is null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return View(plan);
+            }
+        }
+    }
+}
---
+++ GymManagement/DbContexts/GymDbContext.cs
@@ -0,0 +1,20 @@
+﻿using GymManagement.Configurations;
+using GymManagement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymManagement.DbContexts
+{
+    public class GymDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=GymManagementDB;Trusted_Connection=true;TrustServerCertificate=true;");
+        }
+
+        override protected void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration<Plan>(new PlanConfiguration());
+        }
+        public DbSet<Plan> Plans { get; set; }
+    }
+}
